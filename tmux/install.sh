936ed5a0ed10144f0211d55219ad99a6b0a42a90
vunguyenhung <vunguyenhung@outlook.com>
2020-01-12 19:44:26 +0700

    Minor improvement on tmux & Add Utilsnips to neovim

--- tmux/install.sh
+++ tmux/install.sh
@@ -2,7 +2,6 @@ echo "Installing tmux..."
 brew install tmux
 echo "Installed tmux!"
 
-
 echo "Copying configs..."
 cp ./tmux.conf ~/.tmux.conf
 cp ./tmux.paper-color-theme ~/.tmux.paper-color-theme
