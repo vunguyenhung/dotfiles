3fc6f0bcaa8fe1c484da257dc4fb9134d41d2b51
vunguyenhung <vunguyenhung@outlook.com>
2020-01-29 17:53:32 +0700

    Add ripgrep config

--- zsh/install.sh
+++ zsh/install.sh
@@ -10,8 +10,16 @@ echo "Installing ripgrep..."
 brew install ripgrep
 echo "Installed ripgrep!"
 
+echo "Backing up your configs..."
+BACKUP_DIR="~/backup/dotfiles/zsh"
+mkdir -p $BACKUP_DIR
+cp ~/.zshrc $BACKUP_DIR/zshrc
+cp ~/.ripgreprc $BACKUP_DIR/ripgreprc
+echo "You can find your backup files in $BACKUP_DIR"
+
 echo "Copying configs..."
 cp ./zshrc ~/.zshrc
+cp ./ripgreprc ~/.ripgreprc
 echo "Copied configs!"
 
 echo "Applying new zsh configs..."
--- zsh/sync-from-local.sh
+++ zsh/sync-from-local.sh
@@ -1,1 +1,2 @@
 cp ~/.zshrc ./zshrc
+cp ~/.ripgreprc ./ripgreprc
