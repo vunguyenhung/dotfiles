f3a2143b881ddd04aa817c9765d208b8b20c5666
vunguyenhung <vunguyenhung@outlook.com>
2020-11-08 16:49:39 +0700

    General improvement

--- neovim/install.sh
+++ neovim/install.sh
@@ -1,15 +1,11 @@
 echo "Installing neovim..."
 brew install neovim
-echo "Installed neovim!"
 
 echo "Installing vim-plug..."
 bash ./vim-plug.sh
-echo "Installed vim-plug!"
 
 echo "Copying configs..."
 mkdir ~/.config/nvim && cp -r ./configs/* ~/.config/nvim/
-echo "Copied configs!"
 
 echo "Installing Vim Plugins..."
 vi -c ':PlugInstall | :qa'
-echo "Installed Vim Plugins!"
--- zsh/install.sh
+++ zsh/install.sh
@@ -1,26 +1,20 @@
 echo "Installing zsh..."
 brew install zsh
-echo "Installed zsh!"
 
 echo "Installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo "Installed ohmyzsh!"
 
 echo "Installing ripgrep..."
 brew install ripgrep
-echo "Installed ripgrep!"
 
 echo "Backing up your configs..."
 mkdir -p ~/.backup/dotfiles/zsh
 cp ~/.zshrc ~/.backup/dotfiles/zsh/.zshrc
 cp ~/.ripgreprc ~/.backup/dotfiles/zsh/.ripgreprc
-echo "You can find your backup files in ~/.backup/dotfiles/zsh"
 
 echo "Copying configs..."
 cp ./zshrc ~/.zshrc
 cp ./ripgreprc ~/.ripgreprc
-echo "Copied configs!"
 
 echo "Applying new zsh configs..."
 source ~/.zshrc
-echo "Applied new zsh configs..."
