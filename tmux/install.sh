30e31302cdacec01ce6d0a311d0b11babcca34b8
Hung Vu <vunguyenhung@outlook.com>
2021-09-05 08:46:47 +0700

    Add tmux plugins

--- tmux/install.sh
+++ tmux/install.sh
@@ -6,3 +6,8 @@ echo "Copying configs..."
 cp ./tmux.conf ~/.tmux.conf
 cp ./tmux.paper-color-theme ~/.tmux.paper-color-theme
 echo "Copied configs!"
+
+echo "Installing Plugins"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+~/.tmux/plugins/tpm/bin/install_plugins
+echo "Installed Plugins"
