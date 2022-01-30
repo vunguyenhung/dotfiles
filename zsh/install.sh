368b075a961188306a6fd72182d9f9be88c9347b
vunguyenhung <vunguyenhung@outlook.com>
2022-01-30 14:33:17 +0700

    Add zsh theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -4,6 +4,9 @@ brew install zsh
 echo "Installing ohmyzsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+echo "Installing zplug..."
+brew install zplug
+
 echo "Installing plugins..."
 # ripgrep
 brew install ripgrep
