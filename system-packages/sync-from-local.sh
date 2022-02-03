41edf67ae37a653f7c1c4dfbd743b8b2805e78fd
vunguyenhung <vunguyenhung@outlook.com>
2022-02-03 13:09:34 +0700

    Improve system packages sync-from-local script

--- system-packages/sync-from-local.sh
+++ system-packages/sync-from-local.sh
@@ -1,8 +1,2 @@
 mv Brewfile Brewfile-bak
-brew bundle dump
-if [ -f "Brewfile" ]; then
-    rm -f Brewfile-bak
-else 
-    echo "Sync System Packages failed!"
-    mv Brewfile-bak Brewfile
-fi
+brew bundle dump && rm -rf Brewfile-bak
